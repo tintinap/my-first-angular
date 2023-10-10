72b2eef1fd6e0726d48bdac9e415aa1701173a74
tinnapat.plangsri <tinnapat.plangsri@accenture.com>
2023-10-10 22:40:15 +0700

    add topbar and productList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProductListComponent,
+        TopBarComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(
+          [
+            {
+                path: '',
+                component: ProductListComponent
+            },
+            {
+                path: '/nod',
+                component: TopBarComponent
+            }
+          ]
+       )
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
