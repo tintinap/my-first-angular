e2863c5018753fd201bd12f96104417b11b770a1
tinnapat.plangsri <tinnapat.plangsri@accenture.com>
2023-10-14 17:37:28 +0700

    part4: form

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from 'src/app/services/cart.service';
 
 @Component({
@@ -9,13 +10,30 @@ import { CartService } from 'src/app/services/cart.service';
 export class CartComponent implements OnInit {
 
   items:any;
+  checkOutForm:any;
 
   constructor(
-    private cartService: CartService
-  ) {}
+    private cartService: CartService,
+    private formBulder: FormBuilder
+  ) {
+    this.items = this.cartService.getItem();
+
+    this.checkOutForm = this.formBulder.group({
+      name: '',
+      address: ''
+    });
+  }
 
   ngOnInit() {
     this.items = this.cartService.getItem();
   }
 
+
+  onSubmit(customerData:any) {
+    console.warn('your order has been submitted', customerData);
+
+    this.items = this.cartService.clearCart();
+    this.checkOutForm.reset();
+
+  }
 }
